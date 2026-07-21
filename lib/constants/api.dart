02cde077ee5664fa32845e35ed7bf3e8994a0fc8
faizaldr <fdr00zenc@gmail.com>
2026-07-21 10:54:14 +0700

    api url

---
+++ lib/constants/api.dart
@@ -0,0 +1,5 @@
+const String BASE_URL = "http://192.168.56.1:1337";
+const String API_URL = BASE_URL + "/api";
+const String AUTH_URL = API_URL + "/auth";
+const String LOGIN_URL = AUTH_URL + "/local";
+const String REGISTER_URL = LOGIN_URL + "/register";
